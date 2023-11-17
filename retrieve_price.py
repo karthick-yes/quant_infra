31a1332c1c1c206a0ec64694586ca3b1570c67ce
karthick-yes <karthiksunil074@gmail.com>
2023-11-17 15:13:13 +0530

    some edits

--- retrieve_price.py
+++ retrieve_price.py
@@ -1,62 +1,70 @@
 import MySQLdb as mdb
 import yfinance as yf
 import datetime
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
 def connect_to_db():
     db_host = 'localhost'
     db_user = 'me'
     db_pass = 'Tothemoon12utYu'
     db_name = 'price_data'
-    con = create_engine(f"mysql+mysqldb://{db_user}:{db_pass}@{db_host}/{db_name}")
-    return con
+    engine = create_engine(f"mysql+mysqldb://{db_user}:{db_pass}@{db_host}/{db_name}")
+    return engine
 
+def obtain_list_db_tickers(engine):
 
-def obtain_list_db_tickers(con):
-
-    with con:
-        cur = con.cursor()
-        cur.execute("SELECT id, ticker FROM symbol")
-        data = cur.fetchall()
+    with engine.connect() as con:
+        data = con.execute(text("SELECT id, ticker FROM symbol"))
         return [(d[0], d[1]) for d in data]
 
-def get_historical_data_yfinance(ticker : str , start_date = (2000,1,1), end_date = datetime.date.today()):
+def get_historical_data_yfinance(ticker:str , start_date = (2000,1,1), end_date = datetime.date.today()):
 
     prices = []
 
     try:
-        yf_data = yf.download(ticker,start_date, end_date, interval="1d")
+        yf_data = yf.download(ticker,start_date, end_date, interval="1d", ignore_tz=True)
         prices = list(yf_data.to_records())
     except Exception as e:
         print(f"Could not download the price data due to the following error:{e}")
 
     return prices
 
+
 def insert_daily_data(data_vendor_id, symbol_id, daily_price_data, now = datetime.datetime.utcnow()):
 
-    daily_price_data = [ (data_vendor_id, symbol_id, d[0], now, now, d[1], d[2], d[3], d[4], d[5], d[6]) for d in daily_price_data]
+    daily_price_data = [ {"data_vendor_id": data_vendor_id, 
+                          "symbol_id": symbol_id, 
+                          "price_date": d[0], 
+                          "created_date": now, 
+                          "last_updated_date": now, 
+                          "open_price": d[1], 
+                          "high_price": d[2], 
+                          "low_price": d[3], 
+                          "close_price": d[4], 
+                          "volume": d[5], 
+                          "adj_close_price": d[6]} for d in daily_price_data]
 
-    c_str = """data_vendor_id, symbol_id, price_date, created_date,last_updated_date, open_price, high_price, low_price,close_price, volume, adj_close_price"""
+    final_str = "INSERT INTO daily_price (data_vendor_id, symbol_id, price_date, created_date,last_updated_date, open_price, high_price, low_price,close_price, volume, adj_close_price) VALUES (:data_vendor_id, :symbol_id, :price_date, :created_date,:last_updated_date, :open_price, :high_price, :low_price, :close_price, :volume, :adj_close_price)"
 
-    insert_string = ("%s * 11")[:-2]
+    engine = connect_to_db()
+    with engine.connect() as con:
+        for data in daily_price_data:
+            con.execute(text(final_str), data)
+        con.commit()
 
-    final_str = "INSERT INTO daily_price (%s) VALUES (%s)" % (c_str, insert_string)
-
-    con = connect_to_db()
-    with con:
-        cur = con.cursor()
-        cur.executemany(final_str, daily_price_data)
 
 if __name__ == "__main__":
-    con = connect_to_db()
-    tickers = obtain_list_db_tickers(con)
+    engine = connect_to_db()
+    tickers = obtain_list_db_tickers(engine)
     
     for i, ticker in enumerate(tickers):
-        print( f" Adding data for {ticker[1]}: {i+1} out of the {len(tickers)} available.")
+        try:
+            print(f" Adding data for {ticker[1]}: {i+1} out of the {len(tickers)} available.")
+            yf_data = get_historical_data_yfinance(ticker[1])
+            insert_daily_data('1', ticker[0], yf_data)
+        except Exception as e:
+            print(f"Failed to fetch data for {ticker[1]}. Error: {e}")
 
-        yf_data = get_historical_data_yfinance(ticker[1])
-        insert_daily_data('1', ticker[0], yf_data)
-    print("Successfully worked")
     
 
 
--- symbols.py
+++ symbols.py
@@ -2,9 +2,19 @@
 import csv
 import datetime
 import MySQLdb as mdb
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text, delete, MetaData, Table
 
 now = datetime.datetime.utcnow()
+
+def connect_to_db():
+    db_host = 'localhost'
+    db_user = 'me'
+    db_pass = 'Tothemoon12utYu'
+    db_name = 'price_data'
+    engine = create_engine(f"mysql+mysqldb://{db_user}:{db_pass}@{db_host}/{db_name}")
+    return engine
+
+
 #get symbols from the csv file
 def get_symbols():
     symbols = []
@@ -27,27 +37,40 @@ def get_symbols():
 
 
 
-
 def get_list_of_db_tickers(symbols):
-    """
-    Function which would upload the values inside the symbols list to the DB
-    """
     db_host = 'localhost'
     db_user = 'me'
     db_pass = 'Tothemoon12utYu'
     db_name = 'price_data'
     engine = create_engine(f"mysql+mysqldb://{db_user}:{db_pass}@{db_host}/{db_name}")
-    
-   
-    final_str = "INSERT INTO SYMBOL (ticker, instrument, name, sector, currency, created_date, last_updated date) VALUES (:ticker, :instrument, :name, :sector, :currency, :created_date, :last_updated_date)" 
+
+    final_str = "INSERT INTO SYMBOL (ticker, instrument, name, sector, currency, created_date, last_updated_date) VALUES (:ticker, :instrument, :name, :sector, :currency, :created_date, :last_updated_date)"
 
     with engine.connect() as conn:
-        conn.execute(text(final_str), symbols)
+        for symbol in symbols:
+            conn.execute(text(final_str), 
+                         {"ticker": symbol[0], 
+                          "instrument": symbol[1], 
+                          "name": symbol[2], 
+                          "sector": symbol[3], 
+                          "currency": symbol[4], 
+                          "created_date": symbol[5], 
+                          "last_updated_date": symbol[6]})
+        conn.commit()
+
+def clear_tables(engine, table_name:str):
+    metadata_obj = MetaData()
+    table = Table(table_name, metadata_obj,autoload_with=engine)
+    with engine.connect() as conn:
+        delete_stmnt = delete(table)
+        conn.execute(delete_stmnt)
         conn.commit()
 
 
 if __name__ == "__main__":
     symbols = get_symbols()
+    engine = connect_to_db()
+    clear_tables(engine=engine,table_name='symbol')
     get_list_of_db_tickers(symbols)
     print(f"{len(symbols)} symbols were inserted to the database successfully.")
 
